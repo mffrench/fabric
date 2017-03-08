ea85ea8afbfb528e8aa8ae84f20563afaf03e3f7
Mathilde Ffrench <mathilde.ffrench@echinopsii.net>
2017-03-08 21:42:00 +0100

    up reinit script

--- bddtests/scripts/reinit.sh
+++ bddtests/scripts/reinit.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 docker ps -a | grep peer | awk '{print $1}' | xargs docker stop
 docker ps -a | grep peer | awk '{print $1}' | xargs docker rm
+docker ps -a | grep orderer | awk '{print $1}' | xargs docker stop
 docker ps -a | grep orderer | awk '{print $1}' | xargs docker rm
 docker network ls | grep _default | awk '{print $1}' | xargs docker network rm
 cd $GOPATH/src/github.com/hyperledger/fabric/
